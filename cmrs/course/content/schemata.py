321a7c0742fd5c653c48c92bbd1bd48d019053d7
Michael Davis <m.r.davis@me.com>
2011-07-30 17:31:01 +0000

    Portlets folder was deleted and a missing import

--- cmrs/course/content/schemata.py
+++ cmrs/course/content/schemata.py
@@ -1,5 +1,7 @@
 from plone.app.folder.folder import ATFolderSchema
 
+from Products.Archetypes.atapi import Schema
+
 from Products.ATContentTypes.content.schemata import ATContentTypeSchema
 from Products.ATContentTypes.content.schemata import finalizeATCTSchema
 
