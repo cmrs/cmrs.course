eccbddafd819a72c6e90edfad0828f56fd571f2b
Michael Davis <m.r.davis@me.com>
2011-07-31 11:05:59 +0000

    Override the description and add the rename after creation

--- cmrs/course/content/coursefolder.py
+++ cmrs/course/content/coursefolder.py
@@ -5,6 +5,7 @@ from plone.app.folder.folder import ATFolder
 
 from Products.Archetypes.atapi import DisplayList
 from Products.Archetypes.atapi import registerType
+from Products.CMFCore.utils import getToolByName
 
 from cmrs.course.config import PROJECTNAME
 from cmrs.course.interfaces.coursefolder import ICourseFolder
@@ -27,11 +28,32 @@ class CourseFolder(ATFolder):
     def canSetConstrainTypes(self):
         return False
 
+    security.declarePrivate('_renameAfterCreation')
+    def _renameAfterCreation(self, check_auto_id=False):
+        plone_tool = getToolByName(self, 'plone_utils', None)
+        # Can't rename without a subtransaction commit when using
+        # portal_factory!
+        transaction.savepoint(optimistic = True)
+        new_id = self.getCourseCode()
+        new_id = plone_tool.normalizeString(new_id)
+        self.setId(new_id)
+
     security.declarePublic('getCourseAvailabilityVocab')
     def getCourseAvailabilityVocab(self):
         vocab = DisplayList()
         for item in self.getCourseSemesters():
             vocab.add(item, item)
         return vocab
+
+    security.declarePublic('Description')
+    def Description(self):
+        tansform_tool = getToolByName(self, 'portal_transforms')
+        text = self.getText()
+        if not text:
+            return ''
+        text = tansform_tool.convert('html_to_text', text).getData()
+        if len(text) < 300:
+            return text
+        return self.text[:150] + '...'
 
 registerType(CourseFolder, PROJECTNAME)
--- cmrs/course/content/schemata.py
+++ cmrs/course/content/schemata.py
@@ -89,4 +89,5 @@ CourseSchema = ATContentTypeSchema.copy() + Schema((
 ))
 
 CourseSchema['title'].widget.label = 'Course Title'
+AcademicSchema['description'].widget.visible = False
 finalizeATCTSchema(CourseSchema)
