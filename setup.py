12fe0ba98fecb8cd5e8fff5fe405db6c624715d1
Michael Davis <m.r.davis@me.com>
2011-08-22 18:21:28 +0000

    Tidy up the setup.py and readd the entry_points back in

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ long_description = (
     'Download\n'
     '********\n')
 
-tests_require = ['zope.testing']
-
 setup(name='cmrs.course',
       version=version,
       description="Courses for the CMRS website",
@@ -53,10 +51,17 @@ setup(name='cmrs.course',
       namespace_packages=['cmrs', ],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools',
-                        # -*- Extra requirements: -*-
-                        ],
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      test_suite='cmrs.course.tests.test_docs.test_suite',
-      )
+      install_requires=[
+          'setuptools',
+      ],
+      extras_require = {
+          'test': [
+              'plone.app.testing',
+          ]
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
