7b36e89033756d14f9d49613bb11c3186183a15b
Michael Davis <m.r.davis@me.com>
2015-04-05 13:14:42 +0000

    Update description to display first paragraph

--- cmrs/course/content/course.py
+++ cmrs/course/content/course.py
@@ -1,6 +1,7 @@
 import string
 import transaction
 from AccessControl import ClassSecurityInfo
+from lxml import etree
 from OFS.CopySupport import CopyError
 from zope.interface import implements
 
@@ -74,9 +75,10 @@ class Course(ATCTContent):
         text = self.getText()
         if not text:
             return ''
-        text = tansform_tool.convert('html_to_text', text).getData()
-        if len(text) < 300:
-            return text
-        return text[:150] + '...'
+        # wrap the text in a single root element
+        # otherwise lxml throws an error
+        text = '<course>' + text + '</course>'
+        course_xml = etree.fromstring(text)
+        return course_xml[0].text
 
 registerType(Course, PROJECTNAME)
